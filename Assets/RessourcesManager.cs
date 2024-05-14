8ffdfe81e7a1409624b4827fac0c4550e5bc69d5
GolfOcean334 <130740013+GolfOcean334@users.noreply.github.com>
2024-05-14 23:37:40 +0200

    Minor: Ressources qui s'incrémentent :joy:

---
+++ Assets/RessourcesManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class ResourcesManager : MonoBehaviour
+{
+    // Références aux textes UI pour afficher les ressources
+    [SerializeField] private TextMeshProUGUI resource1Text;
+    [SerializeField] private TextMeshProUGUI resource2Text;
+    [SerializeField] private TextMeshProUGUI resource3Text;
+    [SerializeField] private TextMeshProUGUI resource4Text;
+
+    // Compteurs pour les ressources
+    private int resource1 = 0;
+    private int resource2 = 0;
+    private int resource3 = 0;
+    private int resource4 = 0;
+
+    // Vitesse de gain des ressources
+    private readonly int resourcesPerSecond = 5;
+
+    void Start()
+    {
+        // Démarrer la coroutine pour générer des ressources
+        StartCoroutine(GenerateResources());
+    }
+
+    // Coroutine pour générer des ressources
+    IEnumerator GenerateResources()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+
+            // Ajouter des ressources chaque seconde
+            resource1 += resourcesPerSecond;
+            resource2 += resourcesPerSecond;
+            resource3 += resourcesPerSecond;
+            resource4 += resourcesPerSecond;
+
+            // Mettre à jour les textes UI
+            UpdateResourceTexts();
+        }
+    }
+
+    // Méthode pour mettre à jour les textes UI
+    void UpdateResourceTexts()
+    {
+        resource1Text.text = "Wood: " + resource1;
+        resource2Text.text = "Stone: " + resource2;
+        resource3Text.text = "Food: " + resource3;
+        resource4Text.text = "Gemme: " + resource4;
+    }
+}
